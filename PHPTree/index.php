73d42f957a4db7d0972727f5673b0a91d043b183
liuxishun <liuxishun@163.com>
2019-12-11 14:15:13 +0800

    树状插件

---
+++ PHPTree/index.php
@@ -0,0 +1,50 @@
+<meta charset="utf-8" />
+<?php
+// 树状结构 下拉框格式
+require('./PHPTree.class.php');
+$data = array(
+    array(
+        'id' => 1,
+        'name' => '用户管理',
+        'parent_id' => 0
+    ),
+    array(
+        'id' => 2,
+        'name' => '用户列表',
+        'parent_id' => 1
+    ),
+    array(
+        'id' => 3,
+        'name' => '权限管理',
+        'parent_id' => 1
+    ),
+    array(
+        'id' => 4,
+        'name' => '文章管理',
+        'parent_id' => 0
+    ),
+    array(
+        'id' => 5,
+        'name' => '新闻',
+        'parent_id' => 4
+    ),
+    array(
+        'id' => 6,
+        'name' => '国内新闻',
+        'parent_id' => 5
+    )
+);
+
+
+$r = PHPTree::makeTreeForHtml($data);
+
+echo '<h1>PHPTree树形结构</h1>';
+echo '<select style="width:150px;">';
+foreach($r as $item){
+    echo '<option>';
+    echo str_repeat('&nbsp;&nbsp;&nbsp;',$item['level']);
+    echo $item['name'];
+    echo '</option>';
+}
+echo '</select>';
+?>
